0581e96bf0e227435689d0092984ebaf00ec45fb
bryantee <bryantee@gmail.com>
2016-12-18 17:42:13 -0700

    Make items links in sidebar

--- js/components/nav-bar.js
+++ js/components/nav-bar.js
@@ -2,14 +2,12 @@ import React from 'react'
 
 export default function Navbar(props) {
   return (
-    <div>
-      <nav>
-        <div className="nav-wrapper blue-grey darken-4">
-          <div className="brand-logo">
-            React Reader
-          </div>
+    <nav>
+      <div className="nav-wrapper blue-grey darken-4">
+        <div className="brand-logo">
+          React Reader
         </div>
-      </nav>
-    </div>
-  );
+      </div>
+    </nav>
+);
 }
