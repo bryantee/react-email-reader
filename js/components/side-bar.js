67be1fef171b6af0cedcd359af78a0f4bae54649
bryantee <bryantee@gmail.com>
2016-12-19 18:06:10 -0700

    Refactor Sidebar component for greater encapsulation

--- js/components/side-bar.js
+++ js/components/side-bar.js
@@ -1,10 +1,19 @@
 import React from 'react'
+import {Link} from 'react-router'
 
 export default function Sidebar(props) {
-
+  const links = Object.keys(props.links).map((link, index) => {
+    return (
+      <li>
+        <Link to={`${link}`}>
+          {link}
+        </Link>
+      </li>
+      );
+  });
   return (
     <ul id="slide-out" className="side-nav fixed">
-      {props.links}
+      {links}
     </ul>
   );
 }
--- js/components/sidebar-container.js
+++ js/components/sidebar-container.js
@@ -1,19 +1,9 @@
 import React from 'react'
-import {Link} from 'react-router'
 import EMAILS from '../emails'
 import Sidebar from './side-bar'
 
 export default function SidebarContainer(props) {
-  const mailboxes = Object.keys(EMAILS).map((mailbox, index) => {
-    return (
-      <li>
-        <Link to={`${mailbox}`}>
-          {mailbox}
-        </Link>
-      </li>
-      );
-  });
   return (
-    <Sidebar links={mailboxes} />
+    <Sidebar links={EMAILS} />
   );
 }
